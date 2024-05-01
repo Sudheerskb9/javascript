841a161ed4519498c92a9cf05fc79f067b888490
Sudheerskb9 <69882772+Sudheerskb9@users.noreply.github.com>
2024-05-01 04:50:18 +0000

    string2

--- 01_basics/04_string.js
+++ 01_basics/04_string.js
@@ -10,11 +10,29 @@ console.log(gameName.charAt(2))
 
 console.log(gameName.indexOf('i'))
 
+//substring it take out the text from between the range
 const newString = gameName.substring(0, 4)
 console.log(newString)
 
-const anotherString = gameName.slice(-6, 4)
+// slice if there is single parameter its omit before the parameter
+//if there is a range its shows the range between them if there is negative range it range calculte from last
+const anotherString = gameName.slice(-6, 5)
 console.log(anotherString)
 
 //console.log(typeof(gameName))
 
+//trim = used to removing spaces
+const newStringOne = "    hello "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+//replace = is used to replace the text inside the string
+const url = "https:/sudheer%20verma"
+console.log(url.replace("%20", '_'))
+
+// we can also ask that text is present in this string or not with the help of includes
+console.log(url.includes('sudheer'))
+
+//split convert the string into array in the behave of particular parameter
+const stringTwo = "hello everyone how are you"
+console.log(stringTwo.split(' '))
